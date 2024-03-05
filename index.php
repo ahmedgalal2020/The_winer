1c6d7d2b5b47fbc42a0c89b4544f2c9a0b67c15d
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-05 15:46:12 +0100

    add form

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$fname = $_POST['fname'];
+$lname = $_POST['lname'];
+$email = $_POST['email'];
+
+echo $fname . ' ' . $lname . ' ' . $email;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="POST">
+    <input type="text" name="fname" id="fname" placeholder="Vor name">
+    <input type="text" name="lname" id="lname" placeholder="Nach name">
+    <input type="email" name="email" id="" placeholder="Email">
+    <input type="submit" name="submit" value="send">
+    </form>
+</body>
+</html>
