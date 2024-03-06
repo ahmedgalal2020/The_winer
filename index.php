95b010b508be9e6383aae206fc9ab5b4dcea2dda
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-06 12:13:49 +0100

    update

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include './inc/closedb.php';
 
 <div class="position-relative">
 
-        <form action="<?php $_SERVER['PHP_SELF']?>" method="POST" class="position-absolute top-20 start-50 translate-middle">
+        <form action="<?php $_SERVER['PHP_SELF']?>" method="POST" class="position-absolute top-200 start-50 translate-middle">
              <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Vorname</label>
     <input type="text" class="form-control" id="fname" name="fname" aria-describedby="emailHelp" value="<?php echo $fname ?>">
@@ -42,7 +42,7 @@ include './inc/closedb.php';
 
 
     <?php foreach ($users as $user): ?>
-        <div class="card position-absolute top-60 start-50 translate-middle" style="width: 18rem; margin-top:30px" >
+        <div class="card position-absolute top-600 start-50 translate-middle" style="width: 18rem; margin-top:30px" >
         <div class="card-header">
         <?php echo htmlspecialchars($user['fname']) . ' ' . htmlspecialchars($user['lname'])?>
   </div>
