ba71e4fe2324c69ba2f65772ce5a13d7d23c9046
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-07 14:25:40 +0100

    update

--- index.php
+++ index.php
@@ -28,12 +28,10 @@ include './inc/closedb.php';
   </div>
 
   <button type="submit" class="btn btn-primary" name="submit" value="send">Submit</button>
-
+ 
   <?php if(isset($_SESSION['Success'])): ?>
-    <div id="successMessage" class="form-text success"><?php echo $_SESSION['Success']; ?></div>
-    <?php 
-    unset($_SESSION['Success']); 
-    ?>
+    <div id="emailHelp" class="form-text error"><?php echo $_SESSION['Success']; ?></div>
+       <?php unset($_SESSION['Success']); ?>
 <?php endif; ?>
 
 
