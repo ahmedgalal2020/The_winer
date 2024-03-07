68757ca25cccce1d1aa516773285d7c98cbbb8f6
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-07 10:51:12 +0100

    update

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ include './inc/closedb.php';
 </button>
 
 <!-- Modal -->
-<div class="modal-dialog modal-dialog-centered fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
