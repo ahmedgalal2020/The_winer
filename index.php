1d167f61abf5e550170d5084cf1cec3fb11a511c
ahmedgalal2020 <galal.trading2015@gmail.com>
2024-03-07 11:11:20 +0100

    update

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ Der Gewinner
   <div class="modal-dialog modal-dialog-centered">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Der Gewinner ist</h1>
+        <h1 class="modal-title fs-5 text-center" id="staticBackdropLabel">Der Gewinner ist</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <?php foreach ($users as $user): ?>
